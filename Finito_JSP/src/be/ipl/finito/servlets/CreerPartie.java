6c3b8fec56b496cfb14135139c9430f45003a792
anthony.debeule@gmail.com <anthony.debeule@gmail.com@e356c77c-33de-47d4-8261-0fb1bd8ab065>
2013-12-18 16:10:14 +0000

    JSP creerPartie

---
+++ Finito_JSP/src/be/ipl/finito/servlets/CreerPartie.java
@@ -0,0 +1,57 @@
+package be.ipl.finito.servlets;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import be.ipl.finito.domaine.Joueur;
+import be.ipl.finito.domaine.Partie;
+import be.ipl.finito.ucc.GestionJoueur;
+import be.ipl.finito.ucc.GestionPartie;
+
+/**
+ * Servlet implementation class CreerPartie
+ */
+@WebServlet(name = "CreerPartie")
+public class CreerPartie extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	@EJB
+	private GestionJoueur gestionJoueur;
+	@EJB
+	private GestionPartie gestionPartie;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreerPartie() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		synchronized (session) {
+			String pseudo = (String) session.getAttribute("pseudo");
+			Joueur joueur = gestionJoueur.rechercheJoueurViaPseudo(pseudo);
+			Partie partie = gestionPartie.creerPartie(joueur);
+			session.setAttribute("id_partie", partie.getId());
+		}
+		session.getServletContext().getNamedDispatcher("jouer.html").forward(request, response);
+	}
+
+}
