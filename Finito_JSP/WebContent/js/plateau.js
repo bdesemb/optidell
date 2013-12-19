64b7f99871a980472f78168c2ab02d56a015a2a6
marinx.denis@gmail.com <marinx.denis@gmail.com@e356c77c-33de-47d4-8261-0fb1bd8ab065>
2013-12-19 15:16:23 +0000

    reglage du soucis de plateau.js

--- Finito_JSP/WebContent/js/plateau.js
+++ Finito_JSP/WebContent/js/plateau.js
@@ -1,16 +1,33 @@
-function allowDrop(ev)
-{
-ev.preventDefault();
+function allowDrop(ev) {
+	ev.preventDefault();
 }
 
-function drag(ev)
-{
-ev.dataTransfer.setData("Text",ev.target.id);
+function drag(ev) {
+	ev.dataTransfer.setData("Text", ev.target.id);
 }
 
-function drop(ev)
-{
-ev.preventDefault();
-var data=ev.dataTransfer.getData("Text");
-ev.target.appendChild(document.getElementById(data));
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("Text");
+	ev.target.appendChild(document.getElementById(data));
 }
+
+function refresh() {
+	var $request = $.ajax({
+		url: "plateau.html",
+		type: "post",
+	});
+	$request.done(function (response, textStatus, xhr) {
+		if (response.indexOf("form") == -1 && response.indexOf("vainqueur") == -1) {
+			setTimeout(refresh, 200);
+		} 
+		$('#result').html(response);
+	});
+	$request.fail(function (xhr, textStatus, errorThrown) {
+		alert(errorThrown);
+	});
+}
+
+$(function () {
+	refresh();
+});
