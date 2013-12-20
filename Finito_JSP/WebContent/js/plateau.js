9310c24852e03206d3743080dff14c8bfa494260
soryo99@gmail.com <soryo99@gmail.com@e356c77c-33de-47d4-8261-0fb1bd8ab065>
2013-12-20 10:50:50 +0000

    Améliorations plateau

--- Finito_JSP/WebContent/js/plateau.js
+++ Finito_JSP/WebContent/js/plateau.js
@@ -9,12 +9,14 @@ function drag(ev) {
 function drop(ev) {
 	ev.preventDefault();
 	var data = ev.dataTransfer.getData("Text");
-	ev.target.appendChild(document.getElementById(data));
-	var $jetonJoue = $("#plateau").find($(".drag[draggable='true']"));
-	$('#numeroJeton').val($jetonJoue.text());
-	var $case = $(ev.target);
-	$('#idCase').val($case.attr("id"));
-	$("#form").submit();
+	if($(ev.target).attr("occupe") != "true"){
+		ev.target.appendChild(document.getElementById(data));
+		var $jetonJoue = $("#plateau").find($(".drag[draggable='true']"));
+		$('#numeroJeton').val($jetonJoue.text());
+		var $case = $(ev.target);
+		$('#idCase').val($case.attr("id"));
+		$("#form").submit();
+	}
 }
 
 function refresh() {
@@ -24,7 +26,7 @@ function refresh() {
 	});
 	$request.done(function (response, textStatus, xhr) {
 		if (response.indexOf("form") == -1 && response.indexOf("vainqueur") == -1) {
-			setTimeout(refresh, 3000);
+			setTimeout(refresh, 1000);
 		} 
 		$('#affichage').html(response);
 	});
