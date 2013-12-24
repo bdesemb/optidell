8b056ce2edcb4f129e62d57a785c181e88dfc769
soryo99@gmail.com <soryo99@gmail.com@e356c77c-33de-47d4-8261-0fb1bd8ab065>
2013-12-24 13:43:01 +0000

    Le dernier commmmmmit

--- Finito_JSP/WebContent/WEB-INF/jsp/scores.jsp
+++ Finito_JSP/WebContent/WEB-INF/jsp/scores.jsp
@@ -13,36 +13,6 @@
 			<a href="${deconnexion}">Se déconnecter</a>
 		</div>
 	</c:if>
-
-	<%-- <c:forEach var="i" begin="0" end="1">
-		<c:set var="idPlateau" value="plateau${i}" />
-		<div>
-			Le joueur ${ joueursDeLaPartie[i].joueur.prenom} "${joueursDeLaPartie[i].joueur.login}"
-			${joueursDeLaPartie[i].joueur.nom} a fait une suite de
-			<div id="score${i}">${joueursDeLaPartie[i].score}nombre(s).</div>
-		</div>
-		<c:if test="${ joueursDeLaPartie.score == '12' }">
-			<div id="win">Vainqueur</div>
-		</c:if>
-		<table id="${idPlateau }">
-			<c:forEach var="i" begin="0" end="5">
-				<tr>
-					<c:set var="ligneCase" value="${joueursDeLaPartie[i].case[i]}" />
-					<c:set var="ligneJeton" value="${joueursDeLaPartie[i].jeton[i]}" />
-					<c:forEach var="j" begin="0" end="5">
-						<c:set var="case" value="${ligneCase[j]}" />
-						<c:set var="jeton" value="${ligneJeton[j]}" />
-						<td>
-							${ case } : ${ jeton }
-						</td>
-					</c:forEach>
-					<!-- fin forEach colonne -->
-				</tr>
-			</c:forEach>
-			<!-- fin forEach ligne -->
-		</table>
-	</c:forEach> --%>
-	<!-- fin forEach plateau -->
 <table id="page_table">
   <tr>
    <td id="page_td"><div id="contenu">
--- Finito_JSP/src/be/ipl/finito/servlets/AfficherScores.java
+++ Finito_JSP/src/be/ipl/finito/servlets/AfficherScores.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Vector;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -17,15 +16,12 @@ import javax.servlet.http.HttpSession;
 import be.ipl.finito.domaine.Case;
 import be.ipl.finito.domaine.Partie;
 import be.ipl.finito.domaine.Plateau;
-import be.ipl.finito.modeles.JoueurFinDePartie;
 import be.ipl.finito.ucc.GestionJoueur;
 import be.ipl.finito.ucc.GestionPartie;
 import be.ipl.finito.ucc.GestionPlateau;
 
 /**
- * 
- * @author Marjorie
- * 
+ * Servlet implémentation pour afficher les scores
  */
 @WebServlet(name = "afficher_scores.html")
 public class AfficherScores extends HttpServlet {
@@ -49,7 +45,6 @@ public class AfficherScores extends HttpServlet {
 		HttpSession session = request.getSession();
 		int idPartie = (Integer) session.getAttribute("id_partie");
 		List<Plateau> plateauxDeLaPartie = gestionPartie.listerPlateauxEnJeu(gestionPartie.rechercherPartie(idPartie));
-		Vector<JoueurFinDePartie> joueursDeLaPartie = new Vector<JoueurFinDePartie>(3);
 		// On vide déjà un peu la session des attributs plus nécessaires (en rapport avec l'affichage du plateau du joueur).
 		session.removeAttribute("cases");
 		session.removeAttribute("jetonsEnMain");
@@ -57,21 +52,7 @@ public class AfficherScores extends HttpServlet {
 		session.removeAttribute("pasEncoreJoue");
 		session.removeAttribute("phase2");
 		// On fait le traitement nécessaire pour afficher chaque plateau
-		for (Plateau p : plateauxDeLaPartie) {
-			JoueurFinDePartie joueur = new JoueurFinDePartie(p.getJoueur(), gestionPlateau.calculerScore(p));
-			List<Case> casesDuJoueur = gestionPlateau.listerCases(p);
-			for (Case c : casesDuJoueur) {
-				int cpt = 0;
-				if (c.getJeton() != null) {
-					joueur.setJeton(cpt / 6, cpt % 6, c.getJeton().getNumero());
-				}
-				joueur.setCase(cpt / 6, cpt % 6, c.getNumero());
-				cpt++;
-			}
-			joueursDeLaPartie.add(joueur);
-		}
 		synchronized (session) {
-			session.setAttribute("listeJoueurs", joueursDeLaPartie);
 			Partie partie = gestionPartie.rechercherPartie(idPartie);
 			List<Plateau> plateaux = gestionPartie.listerPlateauxEnJeu(partie);
 			Map<Integer, List<Case>> mapPlateaux_idCases = new HashMap<Integer, List<Case>>();
